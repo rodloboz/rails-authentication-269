2f29380d9183822c5c1876d91b41fa1b1d92b58d
Rui Freitas <dinnerpartysaboteur@gmail.com>
2019-07-15 10:04:21 +0200

    rails new with devise user model

---
+++ app/models/user.rb
@@ -0,0 +1,12 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  validates :first_name, :last_name, presence: true
+
+  def greeting
+    "Hi, #{first_name || email}"
+  end
+end
